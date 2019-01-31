4fabce81afefe3498e603604f68356994c5b2427
DanielGuzmanT <jhairguz.t@gmail.com>
2019-01-31 13:19:12 -0500

    solved?

--- validation.py
+++
@@ -1,8 +0,0 @@
-AREAS = ["code", "description", "price_unit", "price_pack"]
-
-
-def validate_create(product):
-    for area in AREAS:
-        if area not in product:
-            return False, "missing {} in request".format(area)
-    return True, "ok"
